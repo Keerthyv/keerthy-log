88b892e68472b32347c2365502fa18ca63225dbd
Keerthy <keerthy.vss@gmail.com>
2023-12-11 23:54:37 -0500

    Add alphabet list at the bottom, display selected alphabet with color

--- projects/wordle/wordle.js
+++ projects/wordle/wordle.js
@@ -7,8 +7,8 @@
 // 4. change to gray if a letter is not present.
 // 5. Check if userInputWord === wordleWord
 
-import { validWordList } from './validWords.js';
-import { wordleList } from './wordleList.js';
+import { validWordList } from "./validWords.js";
+import { wordleList } from "./wordleList.js";
 
 var inputField = document.getElementById("inputWord");
 var inputForm = document.getElementById("puzzle-container");
@@ -25,71 +25,70 @@ var attempt = 1;
 inputForm.addEventListener("submit", submitWord);
 
 function submitWord(event) {
-    event.preventDefault();
-    var userInputWord = event.target["inputWord"].value;
-    console.log(userInputWord);
-    checkAttempts(counter, userInputWord);
-    inputForm.reset();
+  event.preventDefault();
+  var userInputWord = event.target["inputWord"].value;
+  console.log(userInputWord);
+  checkAttempts(counter, userInputWord);
+  inputForm.reset();
 }
 
 function checkAttempts(counter, userInputWord) {
-    if (counter == 0) {
-        userMessage.innerHTML = `You have no attempts left! Today's word was ${wordleWord}`;
-        inputField.disabled = true;
-    } else if (counter > 0) {
-        console.log("Counter: " + counter);
-        console.log("Attempt" + attempt);
-        validateWord(userInputWord);
-    }
+  if (counter == 0) {
+    userMessage.innerHTML = `You have no attempts left! Today's word was ${wordleWord}`;
+    inputField.disabled = true;
+  } else if (counter > 0) {
+    console.log("Counter: " + counter);
+    console.log("Attempt" + attempt);
+    validateWord(userInputWord);
+  }
 }
 
 function validateWord(userInputWord) {
-    //add code to validate if the word is a valid english word
-    userInputWord = userInputWord.toLowerCase();
-    if ((userInputWord.length != 5) || (!(validWordList.includes(userInputWord)))) {
-        userMessage.innerHTML = "Please enter a valid five-letter word.";
-    }
-    else {
-        console.log(userInputWord + " is a valid word");
-        displayClues(userInputWord, counter);
-        counter--;
-        attempt++;
-        console.log("New Counter: " + counter);
-        console.log("New Attempt" + attempt);
-    }
+  //add code to validate if the word is a valid english word
+  userInputWord = userInputWord.toLowerCase();
+  if (userInputWord.length != 5 || !validWordList.includes(userInputWord)) {
+    userMessage.innerHTML = "Please enter a valid five-letter word.";
+  } else {
+    console.log(userInputWord + " is a valid word");
+    displayClues(userInputWord, counter);
+    counter--;
+    attempt++;
+    console.log("New Counter: " + counter);
+    console.log("New Attempt" + attempt);
+  }
 }
 
-
 function displayClues(userInputWord, counter) {
-    var rowId = "row" + counter;
-    console.log(rowId);
-    var inputWordUpperCased = userInputWord.toUpperCase();
-    var word = inputWordUpperCased;
-    console.log(word);
+  var rowId = "row" + counter;
+  console.log(rowId);
+  var inputWordUpperCased = userInputWord.toUpperCase();
+  var word = inputWordUpperCased;
+  console.log(word);
 
-    for (var i = 0; i < word.length; i++) {
-        var currentLetter = word[i];
-        if (wordleWord.includes(word[i]) && word[i] === wordleWord[i]) {
-            console.log("The guessed letter is in the correct position.");
-            displayLetterWithColor(currentLetter, rowId, "green");
-        } else if (wordleWord.includes(word[i])) {
-            console.log("The letter is in the wrong position");
-            displayLetterWithColor(currentLetter, rowId, "yellow");
-        } else if (!wordleWord.includes(word[i])) {
-            console.log("The letter is not in the word");
-            displayLetterWithColor(currentLetter, rowId, "grey");
-        }
+  for (var i = 0; i < word.length; i++) {
+    var currentLetter = word[i];
+    if (wordleWord.includes(word[i]) && word[i] === wordleWord[i]) {
+      console.log("The guessed letter is in the correct position.");
+      displayLetterWithColor(currentLetter, rowId, "green");
+    } else if (wordleWord.includes(word[i])) {
+      console.log("The letter is in the wrong position");
+      displayLetterWithColor(currentLetter, rowId, "yellow");
+    } else if (!wordleWord.includes(word[i])) {
+      console.log("The letter is not in the word");
+      displayLetterWithColor(currentLetter, rowId, "grey");
     }
-    if (word === wordleWord) {
-        userMessage.innerHTML = `You have guessed the word on attempt ${attempt}`;
-        inputField.disabled = true;
-    }
+  }
+  if (word === wordleWord) {
+    userMessage.innerHTML = `You have guessed the word on attempt ${attempt}`;
+    inputField.disabled = true;
+  }
 }
 
 function displayLetterWithColor(letter, rowId, color) {
-    console.log(letter);
-    var letterElement = document.createElement("td");
-    letterElement.innerHTML = `<span class=${color}> ${letter} </span>`;
-    document.getElementById(rowId).appendChild(letterElement);
-    letterElement.classList.toggle(color);
+  console.log(letter);
+  var letterElement = document.createElement("td");
+  letterElement.innerHTML = `<span class=${color}> ${letter} </span>`;
+  document.getElementById(rowId).appendChild(letterElement);
+  letterElement.classList.toggle(color);
+  document.getElementById(letter).classList.add(color);
 }
